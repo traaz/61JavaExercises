f5b369519a44f3e74738da5f07750766b3de6248
traaz <84031734+traaz@users.noreply.github.com>
2021-09-06 11:33:32 +0300

    Update Main.java

--- src/E4/Main.java
+++ src/E4/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		for(int i=0;i<10;i++) {
 			System.out.println("programmer");
 		}
-		//ıkıncı  yol
+		//ikinci  yol
 		System.out.println("************");
 		int a=0;
 		while(a<10) {
