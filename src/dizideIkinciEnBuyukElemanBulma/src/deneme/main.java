d756625fab3702432de89c94a4f0e643d7c1a437
Ali Öztürk <84031734+traaz@users.noreply.github.com>
2022-11-10 01:44:37 +0300

    Update main.java

--- src/dizideIkinciEnBuyukElemanBulma/src/deneme/main.java
+++ src/dizideIkinciEnBuyukElemanBulma/src/deneme/main.java
@@ -4,19 +4,19 @@ public class main {
 
 	public static void main(String[] args) {
 		int[] array= {1,20,5,10};
-		int secondMax=0;
+		int ikinciBuyuk=0;
 		int enBuyuk = array[0];
 		for(int sayi : array) {
 			if(sayi> enBuyuk) {
-				secondMax = enBuyuk;
+				ikinciBuyuk = enBuyuk;
 				enBuyuk = sayi;
 				
 			}
-			else if(sayi>secondMax){
-                secondMax=sayi;
+			else if(sayi>ikinciBuyuk){
+                ikinciBuyuk=sayi;
             }
 		}
-		System.out.println(secondMax);
+		System.out.println(ikinciBuyuk);
 
 	}
 	/*int[] array= {1,20,5,10,100};
